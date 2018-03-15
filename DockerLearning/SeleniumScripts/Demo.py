45b4a77d0fec601b8b9b61f76ff017e2caed7dc5
Stan Ran Zhu <rzhu4@ra.rockwell.com>
2018-03-15 17:23:43 +0800

    update port

--- DockerLearning/SeleniumScripts/Demo.py
+++ DockerLearning/SeleniumScripts/Demo.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from time import sleep
 
 driver = webdriver.Remote(
-command_executor='http://127.0.0.1:32768/wd/hub',
+command_executor='http://127.0.0.1:4444/wd/hub',
 desired_capabilities={'browserName':'chrome'}
 )
 
