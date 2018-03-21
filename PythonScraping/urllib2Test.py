81234d3acdaa158c3ad1236b2a77c9f10dacefb0
Stan Ran Zhu <rzhu4@ra.rockwell.com>
2018-03-21 13:32:47 +0800

    update urllib2test.py

--- PythonScraping/urllib2Test.py
+++ PythonScraping/urllib2Test.py
@@ -1,6 +1,7 @@
 # Basic read html
 
 import urllib2
+import re
 
 def download(url):
     return urllib2.urlopen(url).read()
@@ -19,7 +20,7 @@ def download2(url):
 
 ########################################
 
-# Could retry
+# retry
 def download3(url,num_retries=2):
     print 'Downloading:', url
     try:
@@ -28,11 +29,36 @@ def download3(url,num_retries=2):
         print 'Download error:',e.reason
         html = None
         if num_retries > 0:
-            print e.errno
-            if hasattr(e,'code') and 500 <= e.errno <600:
+            if hasattr(e,'code') and 500 <= e.code <600:
                 # recursively retry 5xx HTTP errors
                 return download3(url, num_retries-1)
     return html
+
+# user agent
+def download4(url,user_agent='wswp',num_retries=2):
+    print 'Downloading:',url
+    headers = {'User-agent':user_agent}
+    request = urllib2.Request(url,headers=headers)
+    try:
+        html = urllib2.urlopen(request).read()
+    except urllib2.HTTPError as e:
+        print 'Download error:', e.reason
+        html = None
+        if num_retries > 0:
+            if hasattr(e,'code') and 500 <= e.code <600:
+                # recursively retry 5xx HTTP errors
+                return download4(url,user_agent,num_retries-1)
+    return html
+
+# Site Map usage
+def crawl_sitemap(url):
+    # download the sitemap file
+    sitemap = download4(url)
+    # extract the sitemap links
+    links = re.findall('<loc>(.*?)</loc>',sitemap)
+    # download each link
+    for link in links:
+        html = download4(link)
 
 # main Test
 if __name__ == '__main__':
