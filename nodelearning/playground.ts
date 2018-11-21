42eec940102444beb98f4f3afe7f5add110dd54f
Stan Ran Zhu <rzhu4@ra.rockwell.com>
2018-11-21 17:12:00 +0800

    Update for the ts file from playground

--- nodelearning/playground.ts
+++ nodelearning/playground.ts
@@ -1,8 +1,8 @@
+//Usage for interface implements.
 interface TestInterface { 
     test_name: string;
     test_age: number;
 }
-
 class TestClass implements TestInterface { 
     test_name = "test";
     test_age = 15;
@@ -13,34 +13,33 @@ class TestClass implements TestInterface {
     }   
 }
 
-let kk = new TestClass();
-let testshot =  kk.testfunc();
-//console.log(testshot);
 
+//Usage for dom operation.
 let node = document.createElement('p');
 let text = document.createTextNode("This is my name");
 node.appendChild(text);
 node.id = 'testP';
 document.body.appendChild(node);
-
 let willChange = document.getElementById('testP');
-
 willChange.hidden = false;
 
-let array_Test: Array<any> = [0, 1, "test"];
 
+//Usage for arrow funciton.
+let array_Test: Array<any> = [0, 1, "test"];
 array_Test.forEach(test_text => console.log(`Text is ${test_text}`));
 
-var a:number;
-while (true) { 
+
+//Usage for While loop
+var a: number;
+while (a == 50) { 
     setInterval(a => Math.random() * 100, 5000);
-    if (a < 50||a>70) {
+    if (a < 50) {
         break;
     }
-    console.log(a);
-
 }
 
+
+//Usage for switch method
 switch (a) { 
     case 1: console.log("1");
         break;
@@ -50,3 +49,24 @@ switch (a) {
         console.log("I am default");
 }
 
+// Add label for function.
+test: function testLabel() {
+
+    console.log("I am label test");
+}
+testLabel();
+
+
+// Add description for Class and Function.
+/**
+* This is test for the class description.
+*/
+class Test { 
+    /**
+    * This is test for the function description.
+    */
+    test() { 
+    }
+}
+
+let t = new Test();
