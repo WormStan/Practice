d5aae284086bc3c411ee81b911cd8cc2043549de
Stan Ran Zhu <rzhu4@ra.rockwell.com>
2018-12-10 14:50:12 +0800

    Update lesson2, use let instead of var

--- nodelearning/Courses/Lesson_2/main.js
+++ nodelearning/Courses/Lesson_2/main.js
@@ -1,7 +1,7 @@
 function addHeader() {
     let headerText = "Lesson 2 -- Data Types & print variable";
-    var node = document.createElement('h1');
-    var text = document.createTextNode(headerText);
+    let node = document.createElement('h1');
+    let text = document.createTextNode(headerText);
     node.appendChild(text);
     document.body.appendChild(node);
 }
