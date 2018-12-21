65eb1426fb050feef951a9acc1600df6ee8d894f
Stan Ran Zhu <rzhu4@ra.rockwell.com>
2018-12-21 09:48:48 +0800

    update for the homework

--- nodelearning/Courses/Homework/Traning_1_9x9/main.js
+++ nodelearning/Courses/Homework/Traning_1_9x9/main.js
@@ -1,4 +1,4 @@
-function addMulTable() {
+function addMultiple() {
     for (let i = 1; i <= 9; i++) {
         let line = "";
         for (let j = 1; j <= i; j++) {
@@ -7,10 +7,10 @@ function addMulTable() {
         document.write(line);
         document.write("<hr>");
     }
-
 }
 
-document.body.onload = addMulTable();
+window.onload = addMultiple();
+
 
 //1. Use JS only for the print out
 //2. Use String features
