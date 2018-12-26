a5329fca7d7bb0c5e31c3d183044b3f634ee977b
Stan Ran Zhu <rzhu4@ra.rockwell.com>
2018-12-26 15:22:55 +0800

    Add async await for testcase

--- nodelearning/Courses/Homework/Training_2_protactor/login_spec.ts
+++ nodelearning/Courses/Homework/Training_2_protactor/login_spec.ts
@@ -1,6 +1,8 @@
 import {browser} from 'protractor';
 import {LoginPage} from './login_page';
 import {LoginData} from './login_data';
+import {element,by} from 'protractor';
+import { async } from 'q';
 
 
 browser.waitForAngularEnabled(false);
@@ -12,11 +14,11 @@ describe("Example for RQM Login Page", ()=>{
     beforeEach(()=>{
         browser.get("https://rqmclm-qa.rockwellautomation.com");
     });
-
-    it('Should login failed via invaild user ID or password', () => {
-        login_page.userName_Input.sendKeys(login_data.invalid_user_name);
-        login_page.userPassword_Input.sendKeys(login_data.invalid_user_passwords);
-        login_page.login_Button.click();
-        expect(login_page.invalidErrorMessage_Text.getText()).toEqual(login_data.invalid_message);
+    it('Should login failed via invaild user ID or password', async () => {
+        await login_page.userName_Input.sendKeys(login_data.invalid_user_name);
+        await login_page.userPassword_Input.sendKeys(login_data.invalid_user_passwords);
+        await login_page.login_Button.click();
+        await expect(login_page.invalidErrorMessage_Text.getText()).toEqual(login_data.invalid_message);
     })
+
 })
